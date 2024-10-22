49a05c8c48fd4bc400e195cddcc974281d08dda5
hoangdv99 <hoangdv257@gmail.com>
2024-10-23 01:06:12 +0900

    :tada: Init vue app

---
+++ web/vite.config.ts
@@ -0,0 +1,21 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    server: {
+      port: parseInt(env.VITE_PORT, 10) || 3000,
+    },
+  }
+})
