44ac1e390f812da6861e6889b578fcbf390cc346
hoangdv99 <hoangdv257@gmail.com>
2024-11-04 23:30:17 +0900

    :card_file_box: Change time column type

--- migrations/000001_create_initial_tables.up.sql
+++ migrations/000001_create_initial_tables.up.sql
@@ -23,8 +23,8 @@ CREATE TABLE IF NOT EXISTS `jax`.`users` (
   `hashed_password` VARCHAR(255) NULL,
   `role` VARCHAR(10) NOT NULL DEFAULT 'user' COMMENT 'user | admin | supervisor',
   `status` INT NOT NULL COMMENT '-10: firestore user\n0: inactive\n10: waiting\n999: active',
-  `created_at` DATETIME NOT NULL,
-  `updated_at` DATETIME NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
 ENGINE = InnoDB;
@@ -39,8 +39,8 @@ CREATE TABLE IF NOT EXISTS `jax`.`stores` (
   `url` VARCHAR(255) NOT NULL,
   `platform` VARCHAR(45) NOT NULL COMMENT '‘woocommerce’ | ‘shopify’ | ‘shopbase’',
   `is_active` TINYINT(1) NOT NULL DEFAULT 1,
-  `created_at` DATETIME NOT NULL,
-  `updated_at` DATETIME NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `url_UNIQUE` (`url` ASC) VISIBLE)
 ENGINE = InnoDB;
@@ -52,8 +52,8 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `jax`.`user_stores` (
   `user_id` INT NOT NULL,
   `store_id` INT NOT NULL,
-  `created_at` DATETIME NOT NULL,
-  `updated_at` DATETIME NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`user_id`, `store_id`),
   INDEX `fk_users_stores_idx` (`user_id` ASC) VISIBLE,
   INDEX `fk_user_stores_stores_idx` (`store_id` ASC) VISIBLE,
@@ -77,8 +77,8 @@ CREATE TABLE IF NOT EXISTS `jax`.`tags` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `ref_id` VARCHAR(255) NULL COMMENT 'Firestore ref ID',
   `name` VARCHAR(45) NOT NULL,
-  `created_at` DATETIME NOT NULL,
-  `updated_at` DATETIME NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE)
 ENGINE = InnoDB;
@@ -91,8 +91,8 @@ CREATE TABLE IF NOT EXISTS `jax`.`user_tags` (
   `user_id` INT NOT NULL,
   `store_id` INT NOT NULL,
   `tag_id` INT NOT NULL,
-  `created_at` DATETIME NOT NULL,
-  `updated_at` DATETIME NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`user_id`, `store_id`, `tag_id`),
   INDEX `fk_user_tags_stores_idx` (`store_id` ASC) VISIBLE,
   INDEX `fk_user_tags_tags_idx` (`tag_id` ASC) VISIBLE,
@@ -122,8 +122,8 @@ CREATE TABLE IF NOT EXISTS `jax`.`tokens` (
   `user_id` INT NOT NULL,
   `expiry` DATETIME NOT NULL,
   `scope` VARCHAR(45) NOT NULL COMMENT 'authentication | activation',
-  `created_at` DATETIME NOT NULL,
-  `updated_at` DATETIME NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`hash`),
   INDEX `fk_tokens_users_idx` (`user_id` ASC) VISIBLE,
   CONSTRAINT `fk_tokens_users`
@@ -142,8 +142,8 @@ CREATE TABLE IF NOT EXISTS `jax`.`blacklist` (
   `store_id` INT NOT NULL,
   `added_by` VARCHAR(255) NOT NULL,
   `scope` VARCHAR(45) NOT NULL,
-  `created_at` DATETIME NOT NULL,
-  `edited_at` DATETIME NOT NULL,
+  `created_at` TIMESTAMP NOT NULL,
+  `edited_at` TIMESTAMP NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_blacklist_stores1_idx` (`store_id` ASC) VISIBLE,
   CONSTRAINT `fk_blacklist_stores1`
