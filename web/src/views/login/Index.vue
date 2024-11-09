f997752a9566a512d29fadc5f3d2782b3b4003a0
hoangdv99 <hoangdv257@gmail.com>
2024-11-09 22:32:50 +0900

    :heavy_plus_sign: Add primevue

---
+++ web/src/views/login/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login-page">
+    <Button label="submit" />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Button from 'primevue/button'
+
+export default defineComponent({
+  name: 'LoginPage',
+  components: {
+    Button,
+  },
+})
+</script>
