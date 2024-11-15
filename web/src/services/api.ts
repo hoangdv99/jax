495a81c9c3ea991e5eed02b4fe01be397f061a1b
hoangdv99 <hoangdv257@gmail.com>
2024-11-15 17:13:53 +0900

    :heavy_plus_sign: Add axios

---
+++ web/src/services/api.ts
@@ -0,0 +1,28 @@
+import axios, { type AxiosResponse } from 'axios'
+
+export interface ApiResponse<T = any> extends AxiosResponse<T> {
+  success: boolean
+}
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_ENDPOINT,
+})
+
+export const api = async (
+  methodType: string,
+  endpoint: string,
+  params: any
+) => {
+  try {
+    const res = await (instance as any)[methodType](endpoint, params)
+    return {
+      ...res,
+      success: true,
+    }
+  } catch (err) {
+    return {
+      data: err,
+      success: false,
+    }
+  }
+}
