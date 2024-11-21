fd2c3ff21e9451be298ddb3c6a02df7937b497ad
hoangdv99 <hoangdv257@gmail.com>
2024-11-21 14:48:43 +0900

    :+1: Add checking auth token expiry

--- web/src/middlewares/auth.ts
+++ web/src/middlewares/auth.ts
@@ -1,4 +1,5 @@
 import type { NavigationGuardNext, RouteLocationNormalized } from 'vue-router'
+import { compareAsc } from 'date-fns'
 
 export const requireAuth = (
   _to: RouteLocationNormalized,
@@ -6,9 +7,18 @@ export const requireAuth = (
   next: NavigationGuardNext
 ) => {
   const token = localStorage.getItem('authToken')
-  if (token) {
+  const expiry = localStorage.getItem('authTokenExpiry') || ''
+
+  if (!token || !expiry) {
+    next({ name: 'Login' })
+  }
+
+  const isValidDate = compareAsc(expiry, new Date()) === 1
+  if (!!token && isValidDate) {
     next()
   } else {
+    localStorage.removeItem('authToken')
+    localStorage.removeItem('authTokenExpiry')
     next({ name: 'Login' })
   }
 }
